c09f146e4c5e3dddeeb3a87f3ebea821d49eec9f
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2017-02-01 17:03:00 +0100

    deposit to ISDC

---
+++ icdeposit.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+import sys
+
+target=sys.argv[1]
+
+isdc_host="isdc-nx00.isdc.unige.ch"
+isdc_root="/home/isdc/savchenk/osa11_deployment/ddcache"
+local_root=os.environ['INTEGRAL_DDCACHE_ROOT']
+
+
+subprocess.check_call([
+                    "ssh",
+                    isdc_host,
+                    "mkdir -p "+isdc_root+"/"+target
+                    ])
+subprocess.check_call([
+                    "rsync",
+                    "-avu",
+                    local_root+"/"+target+"/",
+                    isdc_host+":"+isdc_root+"/"+target,
+                    ])
