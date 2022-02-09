bfdcff204877af01f80624e283c0cef74a262bb7
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-02-09 10:40:55 +0100

    osaic

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
+
 setup(name='osa-ic',
       version='0.1.0',
-      py_modules=['integralicindex','icdeposit'],
+      packages=find_packages(),
       entry_points={
                     "console_scripts": [
                         "osa-ic = osaic.integralicindex:cli"
